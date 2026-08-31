76fdc007a090ece55012239eb3b229ebea1cea33
Amaryllis750 <danielchukwuemela751@gmail.com>
2026-08-31 11:44:48 +0100

    feat: modified color scheme for admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from unfold import admin as UnfoldAdmin
 
 from .models import UserProfile
 
 
 @admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
+class UserProfileAdmin(UnfoldAdmin.ModelAdmin):
     list_display = ("user", "reference_code")
     search_fields = ("user__username", "user__email", "reference_code")
 
